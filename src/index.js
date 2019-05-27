41732a6b6604e33a79d8272daa36f53e5af45fdc
Adrian Frith <adrian@adrianfrith.com>
2019-05-27 14:03:06 +0200

    Generate tile layers for every level and election

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import winston from 'winston'
 import cors from 'cors'
 import morgan from 'morgan'
 import pgConnString from 'pg-connection-string'
+import { Pool, types } from 'pg'
 
 winston.add(new winston.transports.Console())
 
@@ -16,6 +17,10 @@ import lru from 'tilestrata-lru'
 var strata = tilestrata()
 
 const pgConfig = pgConnString.parse(process.env.PG_CONNSTRING)
+const pgPool = new Pool(pgConfig)
+types.setTypeParser(20, parseInt)
+types.setTypeParser(23, parseInt)
+types.setTypeParser(1700, parseFloat)
 
 const lyrOpts = {
   geometry: 'geom',
@@ -24,27 +29,41 @@ const lyrOpts = {
   minZoom: 0,
   maxZoom: 19,
   buffer: 10,
-  resolution: 4096
+  resolution: 4096,
+  table: 'tbl',
+  fields: 'code nat_win_party nat_win_perc prov_win_party prov_win_perc nat_anc prov_anc nat_da prov_da nat_eff prov_eff nat_ifp prov_ifp nat_vfplus prov_vfplus'
 }
 
 const cache = lru({size: '128mb', ttl: 300})
 
-strata.layer('vd_2019').route('tile.mvt')
-  .use(postgismvt({
-    lyr: {
-      ...lyrOpts,
-      table: 'vdl',
-      table_query: 'SELECT * FROM tiles.vd WHERE election_id = 8',
-      fields: 'code nat_win_party nat_win_perc prov_win_party prov_win_perc'
-    },
-    pgConfig
-  }))
-  .use(cache)
+pgPool.query("SELECT e.id, e.code FROM election e JOIN election_type et ON e.type_id = et.id WHERE et.name = 'General Election'")
+  .then(result => {
+    const structureCodes = ['vd', 'ward', 'muni', 'dist', 'prov']
 
-app.use(tilestrata.middleware({
-  server: strata,
-  prefix: '/'
-}))
+    for (const row of result.rows) {
+      for (const struct of structureCodes) {
+        const lyr = {
+          ...lyrOpts,
+          table_query: `SELECT * FROM tiles.${struct} WHERE election_id = ${row.id}`
+        }
+        strata.layer(`${struct}_${row.code}`).route('tile.mvt')
+          .use(postgismvt({
+            lyr,
+            pgConfig
+          }))
+          .use(cache)
+      }
+    }
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {winston.info(`Listening on port ${port}`)})
+    app.use(tilestrata.middleware({
+      server: strata,
+      prefix: '/tiles'
+    }))
+
+    const port = process.env.PORT || 3000
+    app.listen(port, () => {winston.info(`Listening on port ${port}`)})
+  })
+  .catch(err => {
+    console.log('Postgres error', err)
+    process.exit(1)
+  })
